f18faf0cff1bce8485447f9a2f6f7a8376d28093
Suhyeon204 <k0s3y0@naver.com>
2020-05-20 19:26:59 +0900

    help

--- app/src/main/java/com/cookandroid/numberbaseball/MainActivity.java
+++ app/src/main/java/com/cookandroid/numberbaseball/MainActivity.java
@@ -1,13 +1,39 @@
 package com.cookandroid.numberbaseball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int sub = 1002;/*다른 액티비티를 띄우기 위한 요청코드(상수)*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button helpButton = (Button)findViewById(R.id.helpButton);
+        Button startButton = (Button)findViewById(R.id.startButton);
+        //Button endButton = (Button)findViewById(R.id.endButton);
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent helpIntent = new Intent(getApplicationContext(), HelpActivity.class);
+                startActivityForResult(helpIntent,sub);
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/cookandroid/numberbaseball/PlayActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.numberbaseball;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class PlayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
